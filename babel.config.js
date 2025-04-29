8aba89c9b40117c01ecb5ea5a42270e5e6873626
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2025-04-28 23:02:54 -0300

    Usando Drawer

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: ['babel-preset-expo'],//configura o suporte ao JSX automaticamente.
+    plugins: [
+
+        'react-native-reanimated/plugin',
+    ],
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { createDrawerNavigator } from "@react-navigation/drawer"
 // import Home from "../pages/Home";
 import StackRoutes from "../routes/stackRoutes";
 import Sobre from "../pages/Sobre";
@@ -7,62 +6,34 @@ import Contato from "../pages/Contato";
 
 import Feather from "@expo/vector-icons/Feather";
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator()
 
 export default function Routes() {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: true,
-        headerTintColor: "#FF0000",
-        headerStyle: {
-          backgroundColor: "#202225",
-        },
-        tabBarHideOnKeyboard: true, //quando digitar no input e abrir opção de digitar esconde o tabBar
-        tabBarShowLabel: false, //esconde nos nomes da tab no menu
-        tabBarActiveTintColor: "#FF0000", //cor do menu ativo
+    <Drawer.Navigator
 
-        tabBarStyle: {
-          backgroundColor: "#202225",
-          borderTopWidth: 0,
-          height: 100,
-          paddingTop: 10,
-          color: "#FFF",
-        },
-      }}
+      screenOptions={
+        {
+          headerShown: true
+        }
+      }
+
     >
-      <Tab.Screen
+      <Drawer.Screen
         name="HomeStack"
         component={StackRoutes}
-        options={{
-          // tabBarLabel: "",
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => {
-            return <Feather name="home" size={size} color={color} />;
-          },
-        }}
       />
-      <Tab.Screen
+
+      <Drawer.Screen
         name="Sobre"
         component={Sobre}
-        options={{
-          tabBarIcon: ({ color, size }) => {
-            return <Feather name="file-text" color={color} size={size} />;
-          },
-        }}
       />
-      <Tab.Screen
+
+      <Drawer.Screen
         name="Contato"
         component={Contato}
-        options={{
-          title: "Contato",
-          tabBarLabel: "Contato",
-          // headerShown: false,
-          tabBarIcon: ({ color, size }) => {
-            return <Feather name="phone-call" color={color} size={size} />;
-          },
-        }}
+
       />
-    </Tab.Navigator>
+    </Drawer.Navigator>
   );
 }
